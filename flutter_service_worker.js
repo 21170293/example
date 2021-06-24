74b23fcff37634f69a5e9db9a11e56c7edd4e350
Yash kumar shukla <39242081+21170293@users.noreply.github.com>
2021-06-24 22:48:42 +0530

    Update flutter_service_worker.js

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = [
-  "/",
+  "https://21170293.github.io/",
 "main.dart.js",
 "index.html",
 "assets/NOTICES",
